7594594240f0bc819ce997999cd08979bdbe221f
chiragkhurana7777 <chiragkhurana7777@gmail.com>
2026-08-07 20:46:00 +0530

    solved two more ques: 136,182

---
+++ 136.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i:nums){
+            ans^=i;
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
