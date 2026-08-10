e66549d03c738b215fde25a38a0c3e5c92f8dcb6
chiragkhurana7777 <chiragkhurana7777@gmail.com>
2026-08-10 18:20:19 +0530

    solved 922,1748.java

---
+++ 922.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+       int n=nums.length;
+       int j=1;
+
+       for(int i=0;i<n;i+=2){
+        if(nums[i]%2!=0){
+            while(true){
+                if(nums[j]%2==0){
+                    break;
+                }
+                j+=2;
+            }
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+       }
+       return nums;
+       
+       
+
+    }
+}
