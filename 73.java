bc55da48be83bcb6043809d822e11a8bd3d0f56f
chiragkhurana7777 <chiragkhurana7777@gmail.com>
2026-08-18 11:25:12 +0530

    added 73 optimal solution

--- 73.java
+++ 73.java
@@ -28,3 +28,56 @@ class Solution {
 }
 
 //optimal: This is one of those DSA problems where the important part isn't the code — it's realizing that the input matrix itself can be used as your extra storage.
+
+class Solution1 {
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length;
+        int n= matrix[0].length;
+        
+        boolean row=false;
+        boolean col=false;
+
+
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                row=true;
+                break;
+            }    
+        }
+
+        for(int j=0;j<m;j++){
+            if(matrix[j][0]==0){
+                col=true;
+                break;
+            }    
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(row){
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+
+    }
+}
