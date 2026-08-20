c4803dc3f01f96e64e3bcd41c28b165ab712aedb
chiragkhurana7777 <chiragkhurana7777@gmail.com>
2026-08-20 14:53:31 +0530

    solved 560 subarray sum equals k

---
+++ 560.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap <Integer,Integer> map = new HashMap<>();
+        int n=nums.length;
+        int sum=0;
+        int ans=0;
+
+        map.put(0,1);
+        for(int i =0;i<n;i++){
+            sum+=nums[i];
+
+            int needed=sum-k;
+            ans+=map.getOrDefault(needed,0);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+
+        return ans;
+    }
+}
