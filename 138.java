bf0c505287d999d218111d50c2cb30a40ed3bc16
chiragkhurana7777 <chiragkhurana7777@gmail.com>
2026-09-18 02:11:43 +0530

    some changes made

--- 138.java
+++ 138.java
@@ -1,5 +1,5 @@
-/*
-// Definition for a Node.
+import java.util.HashMap;
+
 class Node {
     int val;
     Node next;
@@ -11,7 +11,7 @@ class Node {
         this.random = null;
     }
 }
-*/
+
 
 class Solution {
     public Node copyRandomList(Node head) {
