5a65439a27cb9111e080471af99d03e7e8442a36
chiragkhurana7777 <chiragkhurana7777@gmail.com>
2026-09-18 02:11:13 +0530

    solved 25,83,138

---
+++ 83.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode prev=head;
+        ListNode current=head.next;
+
+        while(current!=null){
+            if(current.val==prev.val){
+                prev.next=current.next;
+            }
+            else{
+                prev=current;
+            }
+            current=current.next;
+        }
+        return head;
+    }
+}
